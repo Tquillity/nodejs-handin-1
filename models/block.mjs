42066bd1bd4a26bafd6135103b38d52904cdcee2
Tquillity <mikael.sundh@gmail.com>
2024-05-03 11:27:39 +0200

    JSDoc comment added for block.mjs

--- models/block.mjs
+++ models/block.mjs
@@ -1,4 +1,16 @@
+/**
+ * Represents a block in a blockchain.
+ */
+
 export default class Block {
+  /**
+   * 
+   * @param {number} index - The index of the block in the blockchain.
+   * @param {string} hash  - The hash of the current Block.
+   * @param {string} previousHash - The has of the previous block.
+   * @param {number} timestamp - The timestamp when the block was created.
+   * @param {*} data - The data stored in the block, for this project a "Complex Object".
+   */
   constructor(
     index,
     hash,
