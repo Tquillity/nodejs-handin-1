ba05378fbe078382cd8618c2f48435d457e145bf
Tquillity <mikael.sundh@gmail.com>
2024-05-08 13:01:46 +0200

    Added sync function to Blockchain controller

--- controllers/blockchain-controller.mjs
+++ controllers/blockchain-controller.mjs
@@ -26,4 +26,39 @@ const createBlock = (req, res, next) => {
   res.status(201).json({ success: true, data: newBlock });
 };
 
-export { createBlock, getBlockchain };
+const syncBlockchain = (req, res, next) => {
+
+  const currentLength = blockchain.chain.length;
+  let maxLength = currentLength;
+  let longestChain = null;
+
+  blockchain.peerNodes.forEach(async (member) => {
+    const response = await fetch(`${member}/api/v1/blockchain`);
+
+    if(response.ok) {
+      const result = await response.json();
+
+      if (result.data.chain.length > maxLength) {
+        maxLength = result.data.chain.length;
+        longestChain = result.data.chain;
+      }
+
+      if (
+        !longestChain ||
+        (longestChain && !blockchain.validateChain(longestChain))
+      ) {
+        console.log('The chain is invalid and syncronized');
+      } else {
+        blockchain.chain = longestChain;
+        console.log(blockchain);
+      }
+    } 
+  });
+  res.status(200).json({
+    success: true,
+    statusCode: 200,
+    data: {message: 'Syncronization complete'},
+  });
+};
+
+export { createBlock, getBlockchain, syncBlockchain };
--- routes/blockchain-routes.mjs
+++ routes/blockchain-routes.mjs
@@ -2,11 +2,13 @@ import express from 'express';
 import {
   createBlock,
   getBlockchain,
+  syncBlockchain,
 } from '../controllers/blockchain-controller.mjs';
 
 const router = express.Router();
 
 router.route('/').get(getBlockchain);
 router.route('/mine').post(createBlock);
+router.route('/consensus').get(syncBlockchain);
 
 export default router;
